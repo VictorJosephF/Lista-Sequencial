40c9a1a86f1f63f9da2ce4733e7dc1f32ed61c14
VictorJosephF <victorfariaj@gmail.com>
2023-03-27 19:45:20 -0300

    exercicios

---
+++ AreaPerimetro.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    double a, area, perimet;
+    Scanner leia = new Scanner(System.in);
+    System.out.print("Digite o comprimento de um lado do quadrado: ");
+    a = leia.nextDouble();
+
+    area = a*a;
+    perimet = a*4;
+
+    System.out.print("O valor da area deste Quadrado é de: "+ area);
+    System.out.print("O valor do perimetro do Quadrado é de: "+perimet);
+    
+    
+  }
+}
