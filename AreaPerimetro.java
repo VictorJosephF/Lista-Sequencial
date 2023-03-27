c379fe5dbf07b52d4b9eea2977cc4ee453ccaaac
VictorJosephF <victorfariaj@gmail.com>
2023-03-27 20:11:07 -0300

    tirando os espaços

--- AreaPerimetro.java
+++ AreaPerimetro.java
@@ -6,13 +6,9 @@ class Main {
     Scanner leia = new Scanner(System.in);
     System.out.print("Digite o comprimento de um lado do quadrado: ");
     a = leia.nextDouble();
-
     area = a*a;
     perimet = a*4;
-
     System.out.print("O valor da area deste Quadrado é de: "+ area);
     System.out.print("O valor do perimetro do Quadrado é de: "+perimet);
-    
-    
   }
 }
