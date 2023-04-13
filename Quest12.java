a690583ee0a3f04b30c9e85b601a8b78ca4b6e07
VICTOR JOSEPH FARIA <68386487+VictorJosephF@users.noreply.github.com>
2023-04-13 16:37:24 -0300

    Create Quest12.java

---
+++ Quest12.java
@@ -0,0 +1,16 @@
+//Tendo como dados de entrada a altura de uma pessoa, construa
+// um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
+import java.util.Scanner;
+public class quest12 {
+    public static void main(String[] args){
+        Scanner read = new Scanner(System.in);
+        double height, weight;
+        System.out.print("Insira sua altura para saber seu peso ideal:  ");
+        height = read.nextFloat();
+
+        weight = (72.7 * height)-58;
+        System.out.print("O peso ideal baseado na sua altura e de: "+ weight);
+    }
+}
+
+//signed by VictorJosephF
